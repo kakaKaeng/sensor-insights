2c4aea2674af876fc4d1fef43700f1823d6d71bf
kakakaeng <kakakaeng@gmail.com>
2025-05-01 12:04:15 +0700

    test ci

--- backend/apps/sensors/models.py
+++ backend/apps/sensors/models.py
@@ -6,3 +6,8 @@
 #     temperature = models.DecimalField(max_digits=5, decimal_places=2)
 #     humidity = models.DecimalField(max_digits=5, decimal_places=2)
 #     air_quality = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+test = "haha"
+
+

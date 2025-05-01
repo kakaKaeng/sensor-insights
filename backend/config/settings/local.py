ddb10bfc7d96e81e34d9d49454c8b953f2531a62
kakakaeng <kakakaeng@gmail.com>
2025-05-01 11:49:38 +0700

    init(backend): setup backend project

---
+++ backend/config/settings/local.py
@@ -0,0 +1,16 @@
+from .base import *  # noqa
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DATABASE_NAME'),
+        'USER': env.str('DATABASE_USER'),
+        'PASSWORD': env.str('DATABASE_PASSWORD'),
+        'HOST': env.str('DATABASE_HOST'),
+        'PORT': env.str('DATABASE_PORT'),
+    }
+}
---
+++ backend/config/settings/production.py
@@ -0,0 +1,12 @@
+from .base import *  # noqa
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DATABASE_NAME'),
+        'USER': env.str('DATABASE_USER'),
+        'PASSWORD': env.str('DATABASE_PASSWORD'),
+        'HOST': env.str('DATABASE_HOST'),
+        'PORT': env.str('DATABASE_PORT'),
+    }
+}

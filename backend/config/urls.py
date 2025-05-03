7d0825db62762192b6544890962d1a3595c95be0
kakakaeng <kakakaeng@gmail.com>
2025-05-03 12:15:27 +0700

    format(bakcend): fix lint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -44,7 +44,11 @@ urlpatterns = [
         schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui',
     ),
-    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path(
+        'api/redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
+    ),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
